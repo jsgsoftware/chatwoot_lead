ce47670b3f919c9334fb4f9e02ea3ba899f69501
johnny <jsgsoftwares@gmail.com>
2025-12-19 12:01:12 -0500

    Modificacion de mensaje de suspension de cuenta

--- app/javascript/dashboard/routes/dashboard/suspended/Index.vue
+++ app/javascript/dashboard/routes/dashboard/suspended/Index.vue
@@ -25,6 +25,17 @@ onMounted(() => {
     <EmptyState
       :title="$t('APP_GLOBAL.ACCOUNT_SUSPENDED.TITLE')"
       :message="$t('APP_GLOBAL.ACCOUNT_SUSPENDED.MESSAGE')"
-    />
+    >
+      <div class="flex justify-center mt-6">
+        <a
+          href="https://api.whatsapp.com/send?phone=50760634535&text=Hola%20quiero%20obtener%20la%20subscripcion%20de%20leadflowai"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded shadow"
+        >
+          Contactar por WhatsApp
+        </a>
+      </div>
+    </EmptyState>
   </div>
 </template>
